ce6794bd95a8b3774847147a021d7650049534b7
Dimorri <Dimorrijones@gmail.com>
2025-07-15 14:50:33 -0400

    Update basicmap.js

--- basicmap.js
+++ basicmap.js
@@ -11,3 +11,8 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
 }).addTo(map);
 
 let centralMarker = L.marker([35.2176665, -80.831473], {title: "CPCC central Campus"}).addTo(map);
+let levineMarker = L.marker([35.11, -80.69], {title: "CPCC Levine Campus"}).addTo(map);
+let catoMarker = L.marker([35.2648627, -80.73116], {title: "CPCC Cato Campus"}).addTo(map);
+let harbonMarker = L.marker([35.12, -80.93], {title: "CPCC Harbon Campus"}).addTo(map);
+let harrisMarker = L.marker([35.22, -80.92], {title: "CPCC Harris Campus"}).addTo(map);
+let merancasMarker = L.marker([35.39, -80.84], {title: "CPCC Mercancas Campus"}).addTo(map);
